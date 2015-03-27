819213f00f66e1cbda07b6bb79eccdfe55066e09
Jeroen Dalsem <jdalsem@coldtrick.com>
2015-03-27 11:15:51 +0100

    check list completeness on item move

--- actions/todo/move.php
+++ actions/todo/move.php
@@ -5,22 +5,35 @@ $container_guid = sanitise_int(get_input('container_guid'), false);
 $pos = sanitise_int(get_input('pos'), false);
 
 $entity = get_entity($guid);
-$container_entity = get_entity($container_guid);
+$new_container_entity = get_entity($container_guid);
+
+$old_container_entity = null;
 
 if ($entity instanceof Todo) {
-	// update container
-	if ($container_entity instanceof Todo) {
-		$entity->container_guid = $container_guid;
-		
-		// move to bottom
-		$entity->order = time();
-		
-		// do after order to clear caches
-		$entity->save();
+	
+	if ($new_container_entity instanceof Todo) {
+		if ($container_guid !== $entity->container_guid) {
+			// moving to a new container
+			
+			$old_container_entity = $entity->getContainerEntity();
+			$entity->container_guid = $container_guid;
+			
+			// move to bottom
+			$entity->order = time();
+			
+			// do after order to clear caches
+			$entity->save();
+		}
 	}
 	
 	// update position
 	$entity->moveToPosition($pos);
+	
+	if ($old_container_entity) {
+		// check list completeness on both lists
+		$old_container_entity->validateListCompleteness();
+		$new_container_entity->validateListCompleteness();
+	}
 } else {
 	register_error('no todo');
 }
