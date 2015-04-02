7d83ef3990c3a448ec5164310efc4347bd18ecea
Jeroen Dalsem <jdalsem@coldtrick.com>
2015-04-02 11:30:22 +0200

    added dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,107 @@
+<?php
+$translation = array(
+  'todos' => 'To-dos',
+  'item:object:todolist' => 'To-do lijst',
+  'item:object:todoitem' => 'To-do item',
+  'todos:settings:enable_groups' => 'Schakel to-dos in voor groepen',
+  'todos:filter:active' => 'Actieve lijsten',
+  'todos:filter:completed' => 'Afgeronde lijsten',
+  'todos:filter:assigned' => 'Toegewezen to-dos',
+  'todos:assigned:closed' => 'Recent gesloten toegewezen to-do items',
+  'todos:assigned:no_results' => 'Er zijn geen toegewezen to-do items gevonden',
+  'todos:all:no_results' => 'Op dit moment zijn er geen to-do lijsten. Door er een aan te maken, kun je starten met het toevoegen van to-dos.',
+  'todos:todolist:add' => 'To-do lijst toevoegen',
+  'todos:todolist:title' => 'Voer de naam in voor de to-do lijst',
+  'todos:todolist:completed' => 'Afgesloten to-dos',
+  'todos:todoitem:add' => 'To-do toevoegen',
+  'todos:todoitem:reopen' => 'Heropen',
+  'todos:todoitem:close' => 'Sluiten',
+  'todos:todoitem:error:missing_container' => 'Ontbrekende to-do lijst',
+  'todos:todoitem:title' => 'Voer de titel van de to-do in',
+  'todos:todoitem:assignee' => 'Toewijzing',
+  'todos:todoitem:due' => 'Einddatum',
+  'todos:sidebar:todoitems_closed:title' => 'Recent gesloten to-do items',
+  'todos:sidebar:todoitems_created:title' => 'Recent toegevoegde to-do items',
+  'todos:widget:assigned:title' => 'Toegewezen to-do items',
+  'todos:sidebar:todoitems_due:title' => 'Geplande to-do items',
+  'todos:widget:assigned:description' => 'Toont een lijst van aan jou toegewezen to-do items',
+  'todos:widget:closed:title' => 'Recent gesloten to-do items',
+  'todos:widget:closed:description' => 'Toont een lijst van recent gesloten to-do items',
+  'todos:widget:closed:none' => 'Geen recent gesloten to-do items',
+  'todos:widget:due:title' => 'Geplande to-do items',
+  'todos:widget:due:description' => 'Toont een lijst van geplande to-do items',
+  'todos:widget:due:none' => 'Er zijn geen to-do items gepland!',
+  'todos:widget:created:title' => 'Recent aangemaakte to-do items',
+  'todos:widget:created:description' => 'Toont een lijst van recent aangemaakte to-do items',
+  'todos:widget:created:none' => 'Geen recent aangemaakte to-do items',
+  'todos:widget:list:title' => 'To-do lijst',
+  'todos:widget:list:description' => 'Toont to-do items van een specifieke to-do lijst',
+  'todos:widget:list:list' => 'Selecteer een lijst om te tonen',
+  'todos:widget:list:select' => 'Selecteer een lijst',
+  'todos:widget:list:no_list' => 'Geen to-do lijst geselecteerd',
+  'todos:group:tool_option' => 'Activeer to-dos',
+  'todos:owner_block:group' => 'Groep to-dos',
+  'todos:notify:todoitem:unassinged:subject' => 'To-do item %s is niet langer toegewezen',
+  'todos:notify:todoitem:unassinged:message' => 'Hallo,
+
+%s heeft to-do item %s niet langer toegewezen aan %s.
+
+Bekijk het to-do item op onderstaande link.
+%s',
+  'todos:notify:todoitem:assinged:subject' => 'To-do item %s is toegewezen',
+  'todos:notify:todoitem:assinged:message' => 'Hallo,
+
+%s heeft to-do item %s toegewezen aan %s.
+
+Bekijk het to-do item op onderstaande link.
+%s',
+  'todos:notify:todoitem:reassinged:subject' => 'To-do item %s is opnieuw toegewezen',
+  'todos:notify:todoitem:reassinged:message' => 'Hallo,
+
+%s heeft de toewijzing van to-do item %s aangepast van %s naar %s.
+
+Bekijk het to-do item op onderstaande link.
+%s',
+  'todos:notify:todoitem:completed:subject' => 'To-do item %s is afgesloten',
+  'todos:notify:todoitem:completed:message' => 'Hallo,
+
+%s heeft to-do item %s afgesloten.
+
+Bekijk het to-do item op onderstaande link.
+%s',
+  'todos:notify:todoitem:reopen:subject' => 'To-do item %s is heropend',
+  'todos:notify:todoitem:reopen:message' => 'Hallo,
+
+%s heeft to-do item %s heropend.
+
+Bekijk het to-do item op onderstaande link.
+%s',
+  'todos:notify:todoitem:due:subject' => 'To-do item %s heeft een nieuwe plan datum',
+  'todos:notify:todoitem:due:message' => 'Hallo,
+
+%s heeft de plan datum van to-do item %s gewijzigd in %s.
+
+Bekijk het to-do item op onderstaande link.
+%s',
+  'todos:notify:todoitem:due_soon:subject' => 'To-do item %s moet snel gesloten worden',
+  'todos:notify:todoitem:due_soon:message' => 'Hallo,
+
+Het to-do item %s moet binnenkort (%s) gesloten worden.
+
+Bekijk het to-do item op onderstaande link.
+%s',
+  'todos:action:error:title' => 'Voer een titel in',
+  'todos:action:todolist:edit:success' => 'To-do lijst opgeslagen',
+  'todos:action:todolist:edit:error' => 'Er is een fout opgetreden bij het opslaan van de to-do lijst',
+  'todos:action:todoitem:edit:cant_write' => 'Het is niet toegestaan to-do items aan deze lijst toe te voegen',
+  'todos:action:todoitem:edit:assignee' => 'Een to-do item mag maar aan 1 persoon worden toegewezen',
+  'todos:action:todoitem:edit:success' => 'To-do item opgeslagen',
+  'todos:action:todoitem:edit:error' => 'Er is een fout opgetreden bij het opslaan van het to-do item',
+  'river:create:object:todoitem' => '%s heeft een to-do %s toegevoegd',
+  'river:close:object:todoitem' => '%s heeft to-do %s gesloten',
+  'river:reopen:object:todoitem' => '%s heeft to-do %s heropend',
+  'todos:todolist:error:missing_container' => 'De container waarin de lijst moet worden opgeslagen ontbreekt',
+  'todos:settings:enable_personal' => 'Schakel persoonlijke to-dos in',
+);
+
+add_translation('nl', $translation);
