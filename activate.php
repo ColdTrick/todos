0fa93d4d7770746bcfd67103c1e2e4443621255c
Jerome Bakker <jeabakker@coldtrick.com>
2017-01-19 15:01:40 +0100

    fixed: subtype class registration

--- activate.php
+++ activate.php
@@ -1,4 +1,13 @@
 <?php
 
-add_subtype('object', TodoList::SUBTYPE, "TodoList");
-add_subtype('object', TodoItem::SUBTYPE, "TodoItem");
+if (get_subtype_id('object', TodoList::SUBTYPE)) {
+	update_subtype('object', TodoList::SUBTYPE, "TodoList");
+} else {
+	add_subtype('object', TodoList::SUBTYPE, "TodoList");
+}
+
+if (get_subtype_id('object', TodoItem::SUBTYPE)) {
+	update_subtype('object', TodoItem::SUBTYPE, "TodoItem");
+} else {
+	add_subtype('object', TodoItem::SUBTYPE, "TodoItem");
+}
